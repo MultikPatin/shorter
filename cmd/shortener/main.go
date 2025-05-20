f874ce2b453b0299a95d5b8e24f45cb564645b60
Dmitry Patin <multik.patin@yandex.ru>
2025-05-20 21:26:39 +0300

    dd bench

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,6 +5,7 @@ import (
 	"main/internal/app"
 	"main/internal/config"
 	"net/http"
+	_ "net/http/pprof"
 )
 
 func main() {
@@ -24,6 +25,14 @@ func main() {
 		"Starting server",
 		"addr", c.Addr,
 	)
+
+	go func() {
+		logger.Infof("PProf endpoints available at /debug/pprof/*")
+		err := http.ListenAndServe(":6060", nil)
+		if err != nil {
+			logger.Errorf("error starting PProf listener:", err)
+		}
+	}()
 
 	if err := http.ListenAndServe(shorterApp.Addr, shorterApp.Router); err != nil {
 		logger.Fatalw(err.Error(), "event", "start server")
