cf3ddc072a2962dbe02ddbf04d6f2bed6abbb911
Dmitry Patin <multik.patin@yandex.ru>
2025-05-23 23:17:18 +0300

    add examples

--- internal/app/links_handlers_test.go
+++ internal/app/links_handlers_test.go
@@ -3,12 +3,9 @@ package app
 import (
 	"bytes"
 	"context"
-	"fmt"
-	"github.com/golang/mock/gomock"
 	"github.com/google/uuid"
 	"main/internal/config"
 	"main/internal/constants"
-	"main/internal/mocks"
 	"main/internal/models"
 	"main/internal/services"
 	"net/http"
@@ -17,129 +14,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
-
-// ExampleLinksHandlers_AddLink demonstrates how to use the AddLink method to add a single link.
-func ExampleLinksHandlers_AddLink() {
-	ctrl := gomock.NewController(&testing.T{})
-	defer ctrl.Finish()
-
-	// Create a mock LinksService.
-	mockLinksService := mocks.NewMockLinksService(ctrl)
-
-	// Define expectation: expect one call to Add with specific parameters.
-	linkToCreate := models.OriginLink{
-		URL: "https://example.com",
-	}
-	resultURL := "http://localhost:8080/test-short-url"
-	mockLinksService.EXPECT().Add(gomock.Any(), linkToCreate, gomock.Any()).Return(resultURL, nil)
-
-	// Create a new LinksHandlers instance using the mock service.
-	handlers := NewLinksHandlers(mockLinksService)
-
-	// Create a simple HTTP request.
-	reqBody := `{"url": "https://example.com"}`
-	request, _ := http.NewRequest(http.MethodPost, "/api/shorten", bytes.NewBufferString(reqBody))
-
-	// Record the response using httptest.
-	recorder := httptest.NewRecorder()
-
-	// Execute the AddLink method.
-	handlers.AddLink(recorder, request)
-
-	fmt.Printf("Response Status: %v\n", recorder.Code)
-	fmt.Printf("Content Type: %v\n", recorder.Header().Get("Content-Type"))
-
-	// Output:
-	// Response Status: 201
-	// Content Type: application/json
-
-}
-
-// ExampleLinksHandlers_AddLinks demonstrates how to use the AddLinks method to add multiple links at once.
-func ExampleLinksHandlers_AddLinks() {
-	ctrl := gomock.NewController(&testing.T{})
-	defer ctrl.Finish()
-
-	// Create a mock LinksService.
-	mockLinksService := mocks.NewMockLinksService(ctrl)
-
-	var originLinks []models.OriginLink
-	for i := 1; i < 3; {
-		url := fmt.Sprintf("https://example%d.com", i)
-		originLink := models.OriginLink{
-			URL: url,
-		}
-		originLinks = append(originLinks, originLink)
-		i++
-	}
-
-	var results []models.Result
-	for i := 1; i < 3; {
-		url := fmt.Sprintf("http://localhost/link%d", i)
-		result := models.Result{
-			Result: url,
-		}
-		results = append(results, result)
-		i++
-	}
-
-	mockLinksService.EXPECT().AddBatch(gomock.Any(), originLinks, gomock.Any()).Return(results, nil)
-
-	// Create a new LinksHandlers instance using the mock service.
-	handlers := NewLinksHandlers(mockLinksService)
-
-	// Create a simple HTTP request.
-	reqBody := `[{"original_url": "https://example1.com"},{"original_url": "https://example2.com"}]`
-	request, _ := http.NewRequest(http.MethodPost, "/api/shorten/batch", bytes.NewBufferString(reqBody))
-
-	// Record the response using httptest.
-	recorder := httptest.NewRecorder()
-
-	// Execute the AddLinks method.
-	handlers.AddLinks(recorder, request)
-
-	fmt.Printf("Response Status: %v\n", recorder.Code)
-	fmt.Printf("Content Type: %v\n", recorder.Header().Get("Content-Type"))
-
-	// Output:
-	// Response Status: 201
-	// Content Type: application/json
-}
-
-// ExampleGetLink demonstrates how to resolve a short link to its original URL.
-func ExampleLinksHandlers_GetLink() {
-	ctrl := gomock.NewController(&testing.T{})
-	defer ctrl.Finish()
-
-	// Create a mock LinksService.
-	mockLinksService := mocks.NewMockLinksService(ctrl)
-
-	// Define expectation: expect one call to Get with specific ID.
-	id := "5a8afeee-412d-4fbe-a059-79e4dc737e1d"
-	originalURL := "https://example.com"
-	mockLinksService.EXPECT().Get(gomock.Any(), id).Return(originalURL, nil)
-
-	// Create a new LinksHandlers instance using the mock service.
-	handlers := NewLinksHandlers(mockLinksService)
-
-	// Create a simple HTTP request.
-	request, _ := http.NewRequest(http.MethodGet, "/"+id+"/", nil)
-	request.SetPathValue("id", id)
-
-	// Record the response using httptest.
-	recorder := httptest.NewRecorder()
-
-	// Execute the GetLink method.
-	handlers.GetLink(recorder, request)
-
-	fmt.Printf("Response Status: %v\n", recorder.Code)
-	fmt.Printf("Content Type: %v\n", recorder.Header().Get("Content-Type"))
-
-	// Output:
-	// Response Status: 307
-	// Content Type: text/plain; charset=utf-8
-
-}
 
 var c = &config.Config{
 	StorageFilePaths: "test.json",
